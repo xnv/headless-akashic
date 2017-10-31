323630180d8cff978b03e048f9654dfe988bf2a6
xnv <xnv@users.noreply.github.com>
2017-11-01 03:20:55 +0900

    chore: small fix on error message

--- polyfill.js
+++ polyfill.js
@@ -1,5 +1,5 @@
 if (typeof global.g !== "undefined")
-	throw new Error("headless-akashic/polyfill-g: `global.g` is already defined.");
+	throw new Error("headless-akashic/polyfill: `global.g` is already defined.");
 Object.defineProperty(global, "g", {
 	value: require("./drivers/akashic-engine.altered"),
 	writable: false
